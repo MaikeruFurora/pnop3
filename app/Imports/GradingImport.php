f6164297e0003ad6caa0fba5e3fcb49d719b689f
bmoqwerty <34005796+bmoqwerty@users.noreply.github.com>
2021-11-26 14:17:40 +0700

    fix in grade import

--- app/Imports/GradingImport.php
+++ app/Imports/GradingImport.php
@@ -35,44 +35,52 @@ class GradingImport implements ToCollection,WithStartRow
         $d4=null;
         $d5=null;
         if ($this->type=="jhs") {
-             foreach($rows as $row){
+            foreach($rows as $row){
 
-           if (!empty($row[0])) {
-            $data= Grade::join('students','grades.student_id','students.id')
-            ->where('students.roll_no',$row[0])
-            ->where('grades.section_id',$this->section)
-            ->where('grades.subject_id',$this->subject)
-            ->first();
-            
-             if (!empty($row[2])) {
-                 $d1=$row[2];
-             }
-             if (!empty($row[3])) {
-                 $d2=$row[3];
-             }
-             if (!empty($row[4])) {
-                 $d3=$row[4];
-             }
-             if (!empty($row[5])) {
-                 $d4=$row[5];
-             }
-             if (!empty($row[6])) {
-                 $d5=$row[6];
-             }
-             
-              Grade::join('students','grades.student_id','students.id')
-             ->where('students.roll_no',$row[0])
-             ->where('grades.section_id',$this->section)
-             ->where('grades.subject_id',$this->subject)
-             ->update([
-                 'grades.first'=>$d1 ?? $data->first,
-                 'grades.second'=>$d2 ?? $data->second,
-                 'grades.third'=>$d3 ?? $data->third,
-                 'grades.fourth'=>$d4 ?? $data->fourth,
-                 'grades.avg'=>(!empty($d5))? $d5 :((!empty($d1) && !empty($d2) && !empty($d3) && !empty($d4))? round(($d1+$d2+$d3+$d4)/4) : $data->avg)
-             ]);
-        
-        }
+            if (!empty($row[0])) {
+                $data= Grade::join('students','grades.student_id','students.id')
+                ->where('students.roll_no',$row[0])
+                ->where('grades.section_id',$this->section)
+                ->where('grades.subject_id',$this->subject)
+                ->first();
+                
+                if (!empty($row[2])) {
+                    $d1=$row[2];
+                }else{
+                    $d1= $data->first;
+                }
+                if (!empty($row[3])) {
+                    $d2=$row[3];
+                }else{
+                    $d2= $data->second;
+                }
+                if (!empty($row[4])) {
+                    $d3=$row[4];
+                }else{
+                    $d3= $data->third;
+                }
+                if (!empty($row[5])) {
+                    $d4=$row[5];
+                }else{
+                    $d4= $data->fourth;
+                }
+                if (!empty($row[6])) {
+                    $d5=$row[6];
+                }
+                
+                Grade::join('students','grades.student_id','students.id')
+                ->where('students.roll_no',$row[0])
+                ->where('grades.section_id',$this->section)
+                ->where('grades.subject_id',$this->subject)
+                ->update([
+                    'grades.first'=>$d1,
+                    'grades.second'=>$d2,
+                    'grades.third'=>$d3,
+                    'grades.fourth'=>$d4,
+                    'grades.avg'=>(!empty($d5))? $d5 :((!empty($d1) && !empty($d2) && !empty($d3) && !empty($d4))? round(($d1+$d2+$d3+$d4)/4) : $data->avg)
+                ]);
+            }
+
        }
         } else {
              foreach($rows as $row){
